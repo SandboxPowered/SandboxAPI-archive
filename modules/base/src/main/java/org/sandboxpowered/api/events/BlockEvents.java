6b8bc8cf1914a2081fa69dfbd9d8debdce974a90
Coded <coded@voxel.games>
2020-10-24 21:19:38 +0100

    Add interact block event

--- modules/base/src/main/java/org/sandboxpowered/api/events/BlockEvents.java
+++ modules/base/src/main/java/org/sandboxpowered/api/events/BlockEvents.java
@@ -1,17 +1,21 @@
 package org.sandboxpowered.api.events;
 
+import org.sandboxpowered.api.entity.player.Hand;
 import org.sandboxpowered.api.entity.player.PlayerEntity;
 import org.sandboxpowered.api.item.ItemStack;
 import org.sandboxpowered.api.state.BlockState;
+import org.sandboxpowered.api.util.InteractionResult;
 import org.sandboxpowered.api.util.math.Position;
 import org.sandboxpowered.api.world.World;
 import org.sandboxpowered.eventhandler.Cancellable;
 import org.sandboxpowered.eventhandler.EventHandler;
+import org.sandboxpowered.eventhandler.Returnable;
 import org.sandboxpowered.internal.InternalService;
 
 public final class BlockEvents {
     public static final EventHandler<BreakEvent> BREAK = InternalService.getInstance().createEventHandler();
     public static final EventHandler<PlaceEvent> PLACE = InternalService.getInstance().createEventHandler();
+    public static final EventHandler<InteractEvent> INTERACT = InternalService.getInstance().createEventHandler();
 
     public interface BreakEvent {
         void onEvent(World world, Position position, BlockState state, PlayerEntity player, ItemStack tool, Cancellable cancellable);
@@ -19,5 +23,9 @@ public final class BlockEvents {
 
     public interface PlaceEvent {
         BlockState onEvent(World world, Position position, BlockState state, PlayerEntity player, ItemStack stack, Cancellable cancellable);
+    }
+
+    public interface InteractEvent {
+        InteractionResult onEvent(World world, Position position, BlockState state, PlayerEntity player, Hand hand, ItemStack stack, InteractionResult result);
     }
 }
