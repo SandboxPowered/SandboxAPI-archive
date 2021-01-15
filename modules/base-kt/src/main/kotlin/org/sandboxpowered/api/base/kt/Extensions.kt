bd208b200c8512401861e1d76c234a9574f8bf3e
Coded <coded@voxel.games>
2021-01-15 17:40:16 +0000

    Add getRegistrarService kotlin extension

--- modules/base-kt/src/main/kotlin/org/sandboxpowered/api/base/kt/Extensions.kt
+++ modules/base-kt/src/main/kotlin/org/sandboxpowered/api/base/kt/Extensions.kt
@@ -3,3 +3,9 @@ package org.sandboxpowered.api.base.kt
 import org.sandboxpowered.api.registry.Registrar
 
 inline fun <reified T: Registrar.Service> Registrar.useRegistrarService(noinline block: (T) -> Unit) = useRegistrarService(T::class.java, block)
+
+inline fun <reified T: Registrar.Service> Registrar.getRegistrarService(): T? {
+    return getRegistrarService(T::class.java).let {
+        if(it.isPresent) it.get() else null
+    }
+}
