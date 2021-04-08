f0facd892b96a3dcec4e19f643a84ffde0252f6b
Ashley Wright <36382780+MsRandom@users.noreply.github.com>
2021-04-08 07:10:07 +0300

    Add kotlin delegate extension to Registry.Entry<T>

--- modules/base-kt/src/main/kotlin/org/sandboxpowered/api/base/kt/Extensions.kt
+++ modules/base-kt/src/main/kotlin/org/sandboxpowered/api/base/kt/Extensions.kt
@@ -8,13 +8,18 @@ import org.sandboxpowered.api.registry.Registrar
 inline fun <reified T : Registrar.Service> Registrar.useRegistrarService(noinline block: (T) -> Unit) =
     useRegistrarService(T::class.java, block)
 
-inline fun <reified T : Registrar.Service> Registrar.getRegistrarService(): T? {
-    return getRegistrarService(T::class.java).let {
-        if (it.isPresent) it.get() else null
-    }
+inline fun <reified T : Registrar.Service> Registrar.getRegistrarService() = getRegistrarService(T::class.java).let {
+    if (it.isPresent) it.get() else null
 }
 
 inline fun <reified T : Component> EntityBlueprint.Builder.add(): EntityBlueprint.Builder = add(T::class.java)
 inline fun <reified T : Component> EntityBlueprint.Builder.remove(): EntityBlueprint.Builder = remove(T::class.java)
 
-inline fun <reified T> FactoryProvider.get(): T = get(T::class.java)
+val <T : Content<T>> Registry.Entry<T>.delegate
+    get() = RegistryEntryDelegate(this)
+
+class RegistryEntryDelegate<T : Content<T>>(private val entry: Registry.Entry<T>) :
+    ReadOnlyProperty<Any?, T?>, () -> T, Registry.Entry<T> by entry {
+    override operator fun invoke(): T = get()
+    override fun getValue(thisRef: Any?, property: KProperty<*>) = entry.orNull()
+}
